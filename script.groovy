6aa9cffebe462bbff412fc2fc91d8375f0d3824c
switchit <72983573+switch2m@users.noreply.github.com>
2022-08-08 13:59:29 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,9 +1,6 @@
 def set_ltsbuild(x) {
     echo 'decrement buildnumber'
-    println(x.getClass())
     def y = x.toInteger() - 1
-    println(y.getClass())
-    println(y)
     return y
 }
 return this
